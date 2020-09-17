6d231a1fd2b3f88a46693ccba90b3d55c6e2d127
KIMBERLY LENSICO <KIMBERLYLENSICO@OUTLOOK.COM>
2020-09-17 16:32:28 -0700

    got the first to-do done

--- class-14/lab/starter-code/js/catalog.js
+++ class-14/lab/starter-code/js/catalog.js
@@ -8,14 +8,24 @@ var cart = new Cart([]);
 // On screen load, we call this method to put all of the busmall options
 // (the things in the Product.allProducts array) into the drop down list.
 function populateForm() {
-
   //TODO: Add an <option> tag inside the form's select for each product
   var selectElement = document.getElementById('items');
-  for (var i in Product.allProducts) {
-
+  var bucket = Product.allProducts;
+  console.log(bucket);
+  for (var i = 0; i < Product.allProducts.length; i++) {
+    var option = document.createElement('option'); // loops through the array of items and creates an element 'option' for items as it loops 
+    option.textContent = bucket[i].name;
+    selectElement.add(option);
+    // console.log(selectElement);
   }
 
 }
+
+
+
+
+populateForm();
+// console.log(populateForm)
 
 // When someone submits the form, we need to add the selected item to the cart
 // object, save the whole thing back to local storage and update the screen
@@ -23,7 +33,7 @@ function populateForm() {
 function handleSubmit(event) {
 
   // TODO: Prevent the page from reloading
-
+  event.preventDefault();
   // Do all the things ...
   addSelectedItemToCart();
   cart.saveToLocalStorage();
@@ -35,12 +45,13 @@ function handleSubmit(event) {
 // TODO: Add the selected item and quantity to the cart
 function addSelectedItemToCart() {
   // TODO: suss out the item picked from the select list
+  var suss = Product.allProducts[i];
   // TODO: get the quantity
   // TODO: using those, add one item to the Cart
 }
 
 // TODO: Update the cart count in the header nav with the number of items in the Cart
-function updateCounter() {}
+function updateCounter() { }
 
 // TODO: As you add items into the cart, show them (item & quantity) in the cart preview div
 function updateCartPreview() {
